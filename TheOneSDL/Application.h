d728131cd3cbac0a2931e2d4a9810d266f61f4cc
Jacob Lowe <jalowe13@gmail.com>
2023-04-08 19:33:43 -0400

    0.20 - Windows and Linux cross compatibility

--- TheOneSDL/Application.h
+++ TheOneSDL/Application.h
@@ -33,7 +33,7 @@ class Physics;
 class Application
 {
 public:
-const char* windowTitle = "C23 Engine: The One SDL v.0.19.4 FPS:";
+const char* windowTitle = "C23 Engine: The One SDL v.0.20 FPS:";
 	Application();
 	~Application();
 

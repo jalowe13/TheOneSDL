39286fb548ba4413a78b4b9cf510559a67166df8
jalowe13 <jalowe13@gmail.com>
2023-07-26 21:57:28 -0500

    0.24.1 - Linux script patch
    - Added git and base-devel package to install script for those who do not have these packages on fresh install

--- TheOneSDL/linux_setup.sh
+++ TheOneSDL/linux_setup.sh
@@ -1,6 +1,17 @@
 # Jacob Lowe
 # Setup for Linux
 
+# This resets the path back to the script file location
+SCRIPT_PATH=$(dirname $(realpath "$0"))
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Installing git and base-devel"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+sudo pacman -Syu --noconfirm base-devel git
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Installing git and base-devel"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+sudo pacman -Syu --noconfirm base-devel git
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Installing yay"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -14,17 +25,17 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Installing Packages..."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 sudo pacman -Syu --noconfirm make boost cmake gcc jsoncpp
-sudo yay -S --noconfirm sdl2-git sdl2_image sdl2_ttf
+sudo yay -S --noconfirm sdl2 sdl2_image sdl2_ttf
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Compiling program for the first time..."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-make
 echo Setting SDL_VIDEODRIVER to wayland or x11
 export SDL_VIDEODRIVER=wayland,x11
 echo The Current Graphics Driver is "$SDL_VIDEODRIVER"
+cd $SCRIPT_PATH
+sudo make
 echo
 echo
-
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Install complete! Press any key to close the window."
 echo "You can now compile program by using the command"
@@ -32,5 +43,5 @@ echo "> make"
 echo "You make also delete all compiled files by typing"
 echo "> make cleanall"
 echo "Launch the game by typing the following or just click the exe in the directory"
-echo "> .\TheOne.exe"
+echo "> ./TheOne"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
